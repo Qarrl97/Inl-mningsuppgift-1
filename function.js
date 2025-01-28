2b2a999f471b683e00665eb0eb33082431ccd866
Qarrl97 <hackmora97@gmail.com>
2025-01-28 15:30:03 +0100

    uppgift1

---
+++ function.js
@@ -0,0 +1,10 @@
+const registerButton=document.getElementById('registerButton');
+const loginButton=document.getElementById('loginButton');
+
+registerButton.addEventListener('click',function() {
+  alert('Registrering lyckades!');
+});
+
+loginButton.addEventListener('click',function() {
+  alert('Inloggning lyckades!');
+});
